d446a67e004676c9784a91a8dc24d80b4964c3a7
L Prashanth <prashanth.py0101@gmail.com>
2024-07-20 22:44:10 +0530

    First

--- src/main/resources/static/js/portfolio.js
+++ src/main/resources/static/js/portfolio.js
@@ -8,3 +8,27 @@ function scrollToPage(button) {
 
     button.classList.add('active');
 }
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("resumeButton").addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/1uoQLBfODh-XOiqjTX5qjBm5rQcv--Yy6/view?usp=sharing", "_blank");
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const titles = ["Java / Spring Boot Developer", "DevOps Engineer"];
+    let currentIndex = 0;
+
+    function cycleTitle() {
+        const titleElement = document.getElementById("title");
+        const titleTextElement = titleElement.querySelector(".title-text");
+
+        currentIndex = (currentIndex + 1) % titles.length;
+        titleTextElement.textContent = titles[currentIndex];
+    }
+
+    setInterval(cycleTitle, 2500);
+
+    document.getElementById("resumeButton").addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/1uoQLBfODh-XOiqjTX5qjBm5rQcv--Yy6/view?usp=sharing", "_blank");
+    });
+});
+
